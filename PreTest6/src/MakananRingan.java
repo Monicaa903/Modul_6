dc55d3d9bc860ad6a79db51e8c5e852c2a172135
Monicaa903 <siscamonica694@gmail.com>
2026-04-07 08:18:33 +0700

    PreTest

---
+++ PreTest6/src/MakananRingan.java
@@ -0,0 +1,14 @@
+package minimarket;
+
+public class MakananRingan extends Produk {
+    private String rasa;
+
+    public MakananRingan(String namaProduk, double harga, String rasa) {
+        super(namaProduk, harga);
+        this.rasa = rasa;
+    }
+    public void tampilkanDetail() {
+        super.tampilkanDetail();
+        System.out.println("Rasa: " + rasa);
+    }
+}
---
+++ PreTest6/src/main.java
@@ -0,0 +1,10 @@
+package minimarket;
+
+public class Main{
+    public static void main(String[] args){
+        MakananRingan snack = new 
+        MakananRingan("Keripik Kentang", 15000, "Barbeque");
+
+        snack.tampilkanDetail();
+    }
+}
